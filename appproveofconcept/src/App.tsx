63c1ed8faa5c5a91d44b77f985f13b461c91e909
Angelo Carlotto <angelocarlotto@gmail.com>
2023-03-31 16:56:43 -0400

    teste

--- appproveofconcept/src/App.tsx
+++ appproveofconcept/src/App.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import { Tweet } from "./components/Tweet";
+import { AppRoutes } from "./AppRoutes";
 
 function App() {
-  return <h1>Hellow world</h1>
+  const [tweets, setTweets] = useState<string[]>([
+    `Tweets 1`,
+    `Tweets 2`,
+    `Tweets 3`,
+    `Tweets 4`,
+  ]);
+  function createTweet() {
+    setTweets([...tweets, `tweet 5`]);
+  }
+  return (
+    <AppRoutes />
+    /* {
+      
+      
+       <div>
+      {tweets.map((tweet) => {
+        return <Tweet text={tweet} />;
+      })}
+
+      <button onClick={createTweet}>Adicionar Tweet</button>
+    </div> }
+    */
+  );
 }
 
-export default App
+export default App;
---
+++ appproveofconcept/src/components/Tweet.tsx
@@ -0,0 +1,6 @@
+type TweetProps = {
+  text: string;
+};
+export function Tweet(props: TweetProps) {
+  return <h1>{props.text}</h1>;
+}
